9613bb61b3e034a2c19c2b5a736c189f19bfc9b5
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2021-05-23 22:50:02 +0300

    Bird Added and Positioned.

--- core/src/com/samet/survivorbird/SurvivorBird.java
+++ core/src/com/samet/survivorbird/SurvivorBird.java
@@ -4,18 +4,23 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class SurvivorBird extends ApplicationAdapter {
 
 	SpriteBatch batch;
 	Texture background;
+	Texture bird;
+	float birdX = 0;
+	float birdY = 0;
 
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		background = new Texture("background.png");
+		bird = new Texture("bird.png");
+		birdX = Gdx.graphics.getWidth() / 2 - Gdx.graphics.getHeight() / 2;
+		birdY = Gdx.graphics.getHeight() / 3;
 
 	}
 
@@ -23,10 +28,9 @@ public class SurvivorBird extends ApplicationAdapter {
 	public void render () {
 
 		batch.begin();
-		batch.draw(background,0,0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-
-
+		batch.draw(bird, birdX, birdY, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
 
 
 		batch.end();
