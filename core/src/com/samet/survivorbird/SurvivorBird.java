2f2cfc63a7d725c0211a9300f4969d047cf03988
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2021-05-23 23:06:09 +0300

    Gravity Created

--- core/src/com/samet/survivorbird/SurvivorBird.java
+++ core/src/com/samet/survivorbird/SurvivorBird.java
@@ -12,6 +12,9 @@ public class SurvivorBird extends ApplicationAdapter {
 	Texture bird;
 	float birdX = 0;
 	float birdY = 0;
+	int gameState = 0;
+	float velocity = 0;
+	float gravity = 0.1f;
 
 
 	@Override
@@ -26,6 +29,26 @@ public class SurvivorBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
+		if (Gdx.input.justTouched()) {
+			gameState = 1;
+		}
+
+		if (gameState == 1) {
+
+			if (birdY > 0 || velocity < 0) {
+
+				velocity = velocity + gravity;
+				birdY = birdY - velocity;
+			}
+
+
+		} else {
+			if (Gdx.input.justTouched()) {
+				gameState = 1;
+			}
+		}
+
 
 		batch.begin();
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
