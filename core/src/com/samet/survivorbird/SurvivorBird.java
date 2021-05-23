162cb2c621f78fdf9a6fac8e7303d51419e95a5c
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2021-05-24 00:10:39 +0300

    Enemy Skulls Can Attack

--- core/src/com/samet/survivorbird/SurvivorBird.java
+++ core/src/com/samet/survivorbird/SurvivorBird.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class SurvivorBird extends ApplicationAdapter {
 
 	SpriteBatch batch;
@@ -18,7 +20,18 @@ public class SurvivorBird extends ApplicationAdapter {
 	int gameState = 0;
 	float velocity = 0;
 	float gravity = 0.1f;
-	float enemyX = 0;
+	float enemyVelocity = 2;
+	Random random;
+
+
+	int numberOfEnemies = 4;
+	float[] enemyX = new float[numberOfEnemies];
+	float[] enemyOffSet = new float[numberOfEnemies];
+	float[] enemyOffSet2 = new float[numberOfEnemies];
+	float[] enemyOffSet3 = new float[numberOfEnemies];
+
+
+	float distance = 0;
 
 
 	@Override
@@ -29,10 +42,22 @@ public class SurvivorBird extends ApplicationAdapter {
 		enemy1 = new Texture("enemyskull.png");
 		enemy2 = new Texture("enemyskull.png");
 		enemy3 = new Texture("enemyskull.png");
-		enemyX = 900;
+
+		distance = Gdx.graphics.getWidth() / 2;
+		random = new Random();
 
 		birdX = Gdx.graphics.getWidth() / 2 - Gdx.graphics.getHeight() / 2;
 		birdY = Gdx.graphics.getHeight() / 3;
+
+		for (int i = 0; i < numberOfEnemies; i++) {
+
+			enemyOffSet[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+			enemyOffSet2[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+			enemyOffSet3[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+
+
+			enemyX[i] = Gdx.graphics.getWidth() - enemy1.getWidth() / 2 + i * distance;
+		}
 
 	}
 
@@ -43,15 +68,32 @@ public class SurvivorBird extends ApplicationAdapter {
 
 		if (gameState == 1) {
 
-			batch.draw(enemy1, enemyX, 50, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
-			batch.draw(enemy2, enemyX, 150, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
-			batch.draw(enemy3, enemyX, 350, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
-
-			enemyX -= 4;
-
 			if (Gdx.input.justTouched()) {
 				velocity = -7;
 			}
+
+			for (int i = 0; i < numberOfEnemies; i++) {
+
+				if (enemyX[i] < Gdx.graphics.getWidth() / 15) {
+
+					enemyX[i] = enemyX[i] + numberOfEnemies * distance;
+					enemyOffSet[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+					enemyOffSet2[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+					enemyOffSet3[i] = (random.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - 200);
+
+
+				} else {
+					enemyX[i] = enemyX[i] - enemyVelocity;
+				}
+
+
+				batch.draw(enemy1, enemyX[i], Gdx.graphics.getHeight() / 2 + enemyOffSet[i], Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+				batch.draw(enemy2, enemyX[i], Gdx.graphics.getHeight() / 2 + enemyOffSet2[i], Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+				batch.draw(enemy3, enemyX[i], Gdx.graphics.getHeight() / 2 + enemyOffSet3[i], Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+
+			}
+
+
 			if (birdY > 0 || velocity < 0) {
 
 				velocity = velocity + gravity + 0.1f;
