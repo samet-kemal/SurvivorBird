f0ebeb4cbf0e6b4107132b9a515d41e22c4447da
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2021-05-23 23:38:44 +0300

    Enemy Skulls Added

--- core/src/com/samet/survivorbird/SurvivorBird.java
+++ core/src/com/samet/survivorbird/SurvivorBird.java
@@ -10,11 +10,15 @@ public class SurvivorBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture bird;
+	Texture enemy1;
+	Texture enemy2;
+	Texture enemy3;
 	float birdX = 0;
 	float birdY = 0;
 	int gameState = 0;
 	float velocity = 0;
 	float gravity = 0.1f;
+	float enemyX = 0;
 
 
 	@Override
@@ -22,6 +26,11 @@ public class SurvivorBird extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		background = new Texture("background.png");
 		bird = new Texture("bird.png");
+		enemy1 = new Texture("enemyskull.png");
+		enemy2 = new Texture("enemyskull.png");
+		enemy3 = new Texture("enemyskull.png");
+		enemyX = 900;
+
 		birdX = Gdx.graphics.getWidth() / 2 - Gdx.graphics.getHeight() / 2;
 		birdY = Gdx.graphics.getHeight() / 3;
 
@@ -29,8 +38,17 @@ public class SurvivorBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
 		if (gameState == 1) {
+
+			batch.draw(enemy1, enemyX, 50, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+			batch.draw(enemy2, enemyX, 150, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+			batch.draw(enemy3, enemyX, 350, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
+
+			enemyX -= 4;
+
 			if (Gdx.input.justTouched()) {
 				velocity = -7;
 			}
@@ -47,9 +65,6 @@ public class SurvivorBird extends ApplicationAdapter {
 			}
 		}
 
-
-		batch.begin();
-		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
 		batch.draw(bird, birdX, birdY, Gdx.graphics.getWidth() / 15, Gdx.graphics.getHeight() / 10);
 
